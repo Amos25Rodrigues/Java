9e64b7d017946c469a9c1a494855001b9b30b444
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-08-26 13:31:48 -0300

    classes

--- classes.java
+++ classes.java
@@ -1,5 +1,20 @@
 public class classes {
     public static void main(String[] args){
         System.out.println("Hello World");
+        Carro meuCarro = new Carro("Corolla");
+        Carro meuCarro2 = new Carro("Civic");
+        Carro meuCarro3 = new Carro("Fastback");
+    }
+
+}
+
+class Carro {
+    String modelo;
+    public Carro(String modelo){
+        this.modelo = modelo;
+    }
+
+    public void acelerar(){
+        System.out.println("Acelerando o carro  " + this.modelo);
     }
 }
