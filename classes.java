74700a33f8e3942805b167d28e95e42bc0396ef9
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-08-27 11:27:49 -0300

    A

--- classes.java
+++ classes.java
@@ -1,5 +1,3 @@
-
-
 public class classes {
     public static void main(String[] args){
         System.out.println("Hello World");
--- exercicios/calculadora.java
+++ exercicios/calculadora.java
@@ -1,5 +1,20 @@
 package exercicios;
-
+import java.util.Scanner;
 public class calculadora {
-    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        double n1 = sc.nextInt();
+
+        System.out.print("Digite mais um numero: ");
+        double n2 = sc.nextInt();
+
+        System.out.println("Qual operação deseja fazer? Ex; +,-,*,/");
+        String operador = sc.nextLine();
+
+
+
+        sc.close();
+    }
 }
