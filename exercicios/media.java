b4ccea35e2ca35502f8eb00dbb3e8979e98e0dfd
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-10-25 12:51:05 -0300

    A

---
+++ exercicios/media.java
@@ -0,0 +1,17 @@
+package exercicios;
+import Java.util.Scanner;
+
+public class media {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new Scanner(System.in);
+
+        System.out.println("Nota 1: ");
+        int n1 = new sc.nextInt()
+        Syste,.out.println("Nota 2: ");
+        int n2 = new sc.nextInt()
+        System.out.println("Nota 3: ");
+        int n3 = new sc.nextInt()
+
+        System.out.println(n1,n2,n3);
+    }
+}
