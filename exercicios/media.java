84734e1eea0ad08c7c02a92c03aa2cc29b5e7e35
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-10-25 13:37:40 -0300

    A

--- exercicios/media.java
+++ exercicios/media.java
@@ -1,17 +1,19 @@
 package exercicios;
-import Java.util.Scanner;
+import java.util.Scanner;
 
 public class media {
     public static void main(String[] args) {
-        java.util.Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Nota 1: ");
-        int n1 = new sc.nextInt()
-        Syste,.out.println("Nota 2: ");
-        int n2 = new sc.nextInt()
-        System.out.println("Nota 3: ");
-        int n3 = new sc.nextInt()
+        System.out.print("Nota 1: ");
+        int n1 = sc.nextInt();
+        System.out.print("Nota 2: ");
+        int n2 = sc.nextInt();
+        System.out.print("Nota 3: ");
+        int n3 = sc.nextInt();
 
-        System.out.println(n1,n2,n3);
+        
+
+        sc.close();
     }
 }
