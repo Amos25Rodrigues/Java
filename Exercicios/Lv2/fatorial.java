ca8efac6c3de33f416c4ea1b24d71212a2a17b8c
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-10-29 11:38:22 -0300

    fatorial

--- Exercicios/Lv2/fatorial.java
+++ Exercicios/Lv2/fatorial.java
@@ -4,11 +4,51 @@ import java.util.Scanner;
 public class fatorial {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite o numero que deseja ver o fatorial: ");;
+        //Forma que eu fiz
+        System.out.print("Digite o numero que deseja ver o fatorial: ");;
         int n = sc.nextInt();
+        int[] fat = new int[n];
+        int n1 = n;
         
-        
+        for (int i = 0; i < n ; i++) {
+            fat[i] = n1;
+            n1--;
+        }
+
+        System.out.println("O Resultado de " + n + "! é:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fat[i]);
+            if (i != n - 1) {
+                System.out.print(" x ");
+            } 
+        }
+
+        System.out.print(" = ");
+
+        int res = 1;
+
+        for (int i = 0; i < n; i++) {
+            res *= fat[i];
+        }
+
+        System.out.println(res);
+
+        // Forma simplificada
+        /*System.out.print("Digite o número que deseja ver o fatorial: ");
+        int n = sc.nextInt();
+
+        int res = 1;
+        System.out.print("O resultado de " + n + "! = ");
+
+        for (int i = n; i >= 1; i--) {
+            res *= i;
+            System.out.print(i);
+            if (i > 1) System.out.print(" x ");
+        }
+
+        System.out.println(" = " + res);
+        */
 
         sc.close();
     }    
