709bb663eef23ad30c88e790b1822bd018f1499b
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-10-29 12:23:41 -0300

    inv

---
+++ Exercicios/Lv2/inversor.java
@@ -0,0 +1,26 @@
+package Exercicios.Lv2;
+import java.util.Scanner;
+
+public class inversor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String[] nomes = new String[5];
+
+        for (int i = 0; i < nomes.length; i++) {
+            if (i == 0) {
+                System.out.print("Digite um nome: ");
+                nomes[i] = sc.nextLine();
+            } else {
+                System.out.print("Digite mais um nome: ");
+                nomes[i] = sc.nextLine();
+            }
+        }
+
+        for (int i = nomes.length - 1 ; i >= 0 ; i--) {
+            System.out.println(nomes[i]);
+        }
+
+        sc.close();
+    }
+}
