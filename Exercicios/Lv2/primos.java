d54697b5712b26403cd2c973b93c32c7caded3e0
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-01 08:23:00 -0300

    primos

---
+++ Exercicios/Lv2/primos.java
@@ -0,0 +1,12 @@
+package Exercicios.Lv2;
+import java.util.Scanner;
+
+public class primos {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        
+
+        sc.close();
+    }
+}
