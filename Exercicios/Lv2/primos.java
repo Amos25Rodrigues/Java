b560a46b37e339130bd146eba3c650cf6b09067f
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-02 16:46:40 -0300

    Modificação

--- Exercicios/Lv2/primos.java
+++ Exercicios/Lv2/primos.java
@@ -5,7 +5,9 @@ public class primos {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        
+        for (int i = 0; i < 100; i++) {
+            
+        }
 
         sc.close();
     }
