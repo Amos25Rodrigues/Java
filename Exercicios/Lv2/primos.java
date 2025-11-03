79f180f63c697332b4669319f98dfe4883e2bf48
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-03 19:54:32 -0300

    interessante (me rendi ao chat)

--- Exercicios/Lv2/primos.java
+++ Exercicios/Lv2/primos.java
@@ -8,15 +8,18 @@ public class primos {
 
         ArrayList<Integer> primos = new ArrayList<>();
 
-        for (int n = 0; n < 20; n++) {
-            for (int j = 2; j <= (n-1); j++) {
-                if (n <= 1) {
+        for (int n = 2; n < 100; n++) {
+            boolean ehPrimo = true;
+            //Confere se o numero é primo ou não
+            for (int j = 2; j < Math.sqrt(n); j++) { //Esse Math.
+                if (n % j == 0) {
+                    ehPrimo = false;
                     break;
-                } else if (n % j == 0) {
-                    break;
-                } else {
-                    primos.add(n);
                 }
+            }
+            //Se for numero primo, ele add na arraylist
+            if (ehPrimo) {
+                primos.add(n);
             }
         }
 
