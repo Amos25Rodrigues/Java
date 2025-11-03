e0a6c3accbf465c7d29a527731e7503c5cd88429
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-03 18:26:02 -0300

    arraylist primos

--- Exercicios/Lv2/primos.java
+++ Exercicios/Lv2/primos.java
@@ -1,9 +1,12 @@
 package Exercicios.Lv2;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class primos {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> primos = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
             
