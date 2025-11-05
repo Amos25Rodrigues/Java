50fcb7afd158b4ccce8d75842ff606e830cc931e
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-05 00:37:37 -0300

    completo

--- Exercicios/Lv3/funcaoSoma.java
+++ Exercicios/Lv3/funcaoSoma.java
@@ -1,8 +1,19 @@
 package Exercicios.Lv3;
+import java.util.Scanner;
 
 public class funcaoSoma {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Digite um numero: ");
+        int n2 = sc.nextInt();
+
+        System.out.print(soma(n1,n2));
+
+        sc.close();
     }
 
     public static int soma(int a, int b){
