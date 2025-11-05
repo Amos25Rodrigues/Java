225f28e95df157046a5ec30cdd72cd2aed466837
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-05 11:15:28 -0300

    Estrutura código

---
+++ Exercicios/Lv3/verificarCPF.java
@@ -0,0 +1,13 @@
+package Exercicios.Lv3;
+import java.util.Scanner;
+
+public class verificarCPF {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+
+
+        sc.close();
+    }
+}
