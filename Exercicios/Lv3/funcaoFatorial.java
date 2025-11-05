820f0299d1dbd95b2986087096d2aaefad7e4776
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-05 01:13:21 -0300

    Primeira parte feita

---
+++ Exercicios/Lv3/funcaoFatorial.java
@@ -0,0 +1,13 @@
+package Exercicios.Lv3;
+import java.util.Scanner;
+
+public class funcaoFatorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um numero: ");
+        int n = sc.nextInt();
+
+        sc.close();
+    }
+}
