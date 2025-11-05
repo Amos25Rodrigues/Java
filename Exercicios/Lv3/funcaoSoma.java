1ef843169db7e411973e4a590166dc6573f84cc2
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-04 23:47:28 -0300

    métodos

---
+++ Exercicios/Lv3/funcaoSoma.java
@@ -0,0 +1,11 @@
+package Exercicios.Lv3;
+
+public class funcaoSoma {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+}
