2f31339bda3e6d41f2c19d5f72ae9557f3bd601d
Amos25Rodrigues <amos25rodrigues@gmail.com>
2025-11-07 12:54:44 -0300

    Estrutura do método de verificação

--- Exercicios/Lv3/verificarCPF.java
+++ Exercicios/Lv3/verificarCPF.java
@@ -2,6 +2,21 @@ package Exercicios.Lv3;
 import java.util.Scanner;
 
 public class verificarCPF {
+
+    public static boolean ValidadeCPF(String cpf) {
+        String d = cpf;
+
+         try {
+            int[] nums = new int[11];
+            for (int i = 0; i < 11; i++) nums[i] = Character.getNumericValue(d.charAt(i));
+
+            
+            return false;
+         } catch (Exception e) {
+            return false;
+         }         
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
@@ -12,4 +27,5 @@ public class verificarCPF {
 
         sc.close();
     }
+
 }
