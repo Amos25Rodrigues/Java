1950a10e885e436825e730175343c6df828a5a5d
Amos25Rodrigues <amos25rodrigues@gmail.com>
2026-01-29 11:02:33 -0300

    a

---
+++ Aleatorio/Main.java
@@ -0,0 +1,7 @@
+package Aleatorio;
+
+public class Main {
+    public static void main (String[] args){
+        Pessoa p = new Pessoa("Amós", "darknerdams@gmail.com", "(94) 9 9149-9613");
+    }
+}
---
+++ Aleatorio/Pessoa.java
@@ -0,0 +1,11 @@
+package Aleatorio;
+
+public class Pessoa {
+    private String nome;
+    private String email;
+    private String telefone;
+    
+    Pessoa(String nome, String email, String Telefone){
+        
+    }
+}
