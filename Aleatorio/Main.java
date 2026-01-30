ee00a783ccc5b0b8dcfdb8f240362e7b9e2ab04c
Amos25Rodrigues <amos25rodrigues@gmail.com>
2026-01-30 09:03:34 -0300

    nomes

--- Aleatorio/Main.java
+++ Aleatorio/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         Pessoa p = new Pessoa();
 
-        System.out.print("Seu nome: ");
+        System.out.print("--------------------------------\nSeu nome: ");
         p.setNome(sc.nextLine());
         //System.out.println("Seu nome é " + p.getNome() + "!");
 
@@ -17,7 +17,7 @@ public class Main {
         System.out.print("Seu telefone: ");
         p.setTelefone(sc.nextLine());
 
-        System.out.println("\nSeu nome: " + p.getNome() + "\nSeu email: " + p.getEmail() + "\nSeu telefone: " + p.getTelefone() + "\n");
+        System.out.println("--------------------------------\nSeu nome: " + p.getNome() + "\nSeu email: " + p.getEmail() + "\nSeu telefone: " + p.getTelefone() + "\n--------------------------------");
 
         sc.close();
     }
